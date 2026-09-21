311b7435c06aa142ec8e0d12a38b2e62e9ef7a65
trainer-z <298007009+trainer-z@users.noreply.github.com>
2026-09-20 20:00:05 -0500

    Funcion resta

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def funcion_suma(numero1,numero2):
     resultado = numero1 + numero2
     return resultado
 
+
+def funcion_resta(numero1,numero2):
+    resultado = numero1 - numero2
+    return resultado
